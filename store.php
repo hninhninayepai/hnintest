118d943367100c84bac73a68757e24ada0d957db
eieishwezin25 <hninhinaye@aceplussolutons.com>
2017-08-24 21:25:38 +0630

    all data delete in store.php

--- store.php
+++ store.php
@@ -1,18 +1,3 @@
 <?php
-require_once('db.php');
-$name=$_POST['emp_name'];
-$add=$_POST['emp_address'];
-$salary=$_POST['emp_salary'];
-
-$sql="INSERT INTO emp(emp_name,emp_address,emp_salary) VALUES('$name','$add','$salary')";
-$res=mysqli_query($con,$sql);
-
-$current_id=mysqli_insert_id($con);
-if(!empty($current_id)){
-	echo "Can inserted data";
-}else{
-	die("cannot inserted".mysqli_error($con));
-}
-header("Location:index.php");
 
 ?>
