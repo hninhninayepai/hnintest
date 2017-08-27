324a404057e565bf22c15e9c37b35ad4eb2f0d7b
eieishwezin25 <hninhinaye@aceplussolutons.com>
2017-08-27 21:01:38 +0630

    testing

--- store1.php
+++ store1.php
@@ -1,6 +1,7 @@
 <?php
 require_once('db.php');
 $sql="INSERT INTO person(name,age,address,email,phone) VALUES('".$_POST['name']."','".$_POST['age']."','".$_POST['address']."','".$_POST['email']."','".$_POST['phone']."')";
-$res=mysqli_query($con,$sql);
+echo "testing";
 header("Location:index1.php");
+
 ?>
